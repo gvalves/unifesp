f82c625d63f1f2cdc56591fe7874dcd7bdd1a89f
gvalves <gva@geekcode.com.br>
2021-03-03 13:25:35 -0300

    feat(data-structure/registration_with_binary_tree): tree self-balance after insert operation

--- data-structure/registration_with_binary_tree/src/AVLTree3.c
+++ data-structure/registration_with_binary_tree/src/AVLTree3.c
@@ -2,28 +2,40 @@
 #include <stdio.h>
 #include <AVLTree3.h>
 
-Node *insert_node(Node *node, unsigned long key, void *value)
+Node *append_node(Node *node, Node *child_node);
+Node *get_unbalanced_node(Node *node);
+void node_left_rotation(Node *node);
+void node_right_rotation(Node *node);
+
+Node *create_node(unsigned long key, void *value)
 {
-  if (node == 0)
-  {
-    return 0;
-  }
-  Node *new_node, *cur_node;
+  Node *new_node;
 
   new_node = malloc(sizeof(Node));
   new_node->key = key;
   new_node->value = value;
+  new_node->height = 0;
 
-  cur_node = node;
+  return new_node;
+}
+
+Node *append_node(Node *node, Node *child_node)
+{
+  if (node == 0 || child_node == 0)
+  {
+    return 0;
+  }
+  Node *cur_node = node;
 
   while (1)
   {
-    if (new_node->key < cur_node->key)
+    cur_node->height += 1;
+    if (child_node->key < cur_node->key)
     {
       if (cur_node->left_child == 0)
       {
-        cur_node->left_child = new_node;
-        new_node->parent = cur_node;
+        cur_node->left_child = child_node;
+        child_node->parent = cur_node;
         break;
       }
       cur_node = cur_node->left_child;
@@ -32,11 +44,58 @@ Node *insert_node(Node *node, unsigned long key, void *value)
     {
       if (cur_node->right_child == 0)
       {
-        cur_node->right_child = new_node;
-        new_node->parent = cur_node;
+        cur_node->right_child = child_node;
+        child_node->parent = cur_node;
         break;
       }
       cur_node = cur_node->right_child;
+    }
+  }
+
+  return child_node;
+}
+
+Node *insert_node(Node *node, unsigned long key, void *value)
+{
+  if (node == 0)
+  {
+    return 0;
+  }
+  Node *new_node, *root_node;
+
+  new_node = create_node(key, value);
+  root_node = get_root_node(node);
+
+  append_node(node, new_node);
+
+  if (!(abs(get_node_balance(root_node)) <= 1))
+  {
+    Node *unbalanced_node = get_unbalanced_node(new_node->parent);
+
+    if (get_node_balance(unbalanced_node) < 0)
+    {
+
+      if (unbalanced_node->right_child == 0)
+      {
+        node_right_rotation(unbalanced_node);
+      }
+      else
+      {
+        node_left_rotation(unbalanced_node->right_child);
+        node_right_rotation(unbalanced_node);
+      }
+    }
+    else
+    {
+      if (unbalanced_node->left_child == 0)
+      {
+        node_left_rotation(unbalanced_node);
+      }
+      else
+      {
+        node_right_rotation(unbalanced_node->left_child);
+        node_left_rotation(unbalanced_node);
+      }
     }
   }
 
@@ -83,7 +142,8 @@ void remove_node(Node *node, unsigned long key)
   {
     return;
   }
-  Node *cur_node = find_node(node, key);
+  Node *cur_node, *ancestor;
+  cur_node = find_node(node, key);
 
   while (1)
   {
@@ -97,6 +157,15 @@ void remove_node(Node *node, unsigned long key)
       {
         cur_node->parent->right_child = 0;
       }
+
+      ancestor = cur_node->parent;
+
+      while (ancestor != 0)
+      {
+        ancestor->height -= 1;
+        ancestor = ancestor->parent;
+      }
+
       break;
     }
     else if (cur_node->left_child == 0 ^ cur_node->right_child == 0)
@@ -127,11 +196,20 @@ void remove_node(Node *node, unsigned long key)
           cur_node->left_child->parent = cur_node->parent;
         }
       }
+
+      ancestor = cur_node->parent;
+
+      while (ancestor != 0)
+      {
+        ancestor->height -= 1;
+        ancestor = ancestor->parent;
+      }
+
       break;
     }
     else
     {
-      Node *successor = left_most_node(cur_node);
+      Node *successor = get_left_most_node(cur_node);
 
       cur_node->key = successor->key;
       cur_node->value = successor->value;
@@ -145,7 +223,7 @@ void remove_node(Node *node, unsigned long key)
   free(cur_node);
 }
 
-Node *left_most_node(Node *node)
+Node *get_left_most_node(Node *node)
 {
   if (node == 0)
   {
@@ -161,7 +239,7 @@ Node *left_most_node(Node *node)
   return cur_node;
 }
 
-Node *right_most_node(Node *node)
+Node *get_right_most_node(Node *node)
 {
   if (node == 0)
   {
@@ -177,18 +255,122 @@ Node *right_most_node(Node *node)
   return cur_node;
 }
 
-int main(int argc, char *argv[])
+Node *get_root_node(Node *node)
 {
-  Node *root = malloc(sizeof(Node));
-
-  for (size_t i = 0; i < 10000; i++)
+  if (node == 0)
   {
-    insert_node(root, i, 0);
+    return 0;
+  }
+  Node *root_node = node;
+
+  while (root_node->parent != 0)
+  {
+    root_node = root_node->parent;
   }
 
-  Node *node = find_node(root, 84);
+  return root_node;
+}
 
-  printf("key: %lu;pkey: %lu\n", node->key, right_most_node(node)->key);
+int get_node_balance(Node *node)
+{
+  if (node == 0)
+  {
+    return 0;
+  }
+  int l_height, r_height;
 
-  return 0;
+  l_height = node->left_child != 0 ? node->left_child->height : -1;
+  r_height = node->right_child != 0 ? node->right_child->height : -1;
+
+  return l_height - r_height;
+}
+
+Node *get_unbalanced_node(Node *node)
+{
+  if (node == 0)
+  {
+    return 0;
+  }
+  Node *cur_node = node;
+
+  while (abs(get_node_balance(cur_node)) <= 1)
+  {
+    cur_node = cur_node->parent;
+    if (cur_node == 0)
+    {
+      break;
+    }
+  }
+
+  return cur_node;
+}
+
+void node_left_rotation(Node *node)
+{
+  if (node == 0 || node->right_child == 0)
+  {
+    return;
+  }
+  Node *child_node, *parent_node;
+  child_node = node->right_child;
+  parent_node = node->parent;
+
+  if (child_node->left_child != 0)
+  {
+    append_node(node, child_node->left_child);
+  }
+  child_node->left_child = node;
+  child_node->parent = parent_node;
+
+  node->parent = child_node;
+
+  if (parent_node->left_child == node)
+  {
+    parent_node->left_child = child_node;
+  }
+  else
+  {
+    parent_node->right_child = child_node;
+  }
+
+  if (child_node->height == 0)
+  {
+    child_node->height += 1;
+  }
+  node->height += node->height == 1 ? -1 : -2;
+}
+
+void node_right_rotation(Node *node)
+{
+  if (node == 0 || node->left_child == 0)
+  {
+    return;
+  }
+  Node *child_node, *parent_node;
+  child_node = node->left_child;
+  parent_node = node->parent;
+
+  if (child_node->right_child != 0)
+  {
+    append_node(node, child_node->right_child);
+  }
+  child_node->right_child = node;
+  child_node->parent = parent_node;
+
+  node->parent = child_node;
+
+  if (parent_node->left_child == node)
+  {
+    parent_node->left_child = child_node;
+  }
+  else
+  {
+    parent_node->right_child = child_node;
+  }
+
+  if (child_node->height == 0)
+  {
+    child_node->height += 1;
+  }
+  node->height += node->height == 1 ? -1 : -2;
 }
--- data-structure/registration_with_binary_tree/src/AVLTree3.h
+++ data-structure/registration_with_binary_tree/src/AVLTree3.h
@@ -8,12 +8,16 @@ typedef struct _Node
   struct _Node *left_child;
   struct _Node *right_child;
   struct _Node *parent;
+  int height;
 } Node;
 
+Node *create_node(unsigned long key, void *value);
 Node *insert_node(Node *node, unsigned long key, void *value);
 Node *find_node(Node *node, unsigned long key);
 void remove_node(Node *node, unsigned long key);
-Node *left_most_node(Node *node);
-Node *right_most_node(Node *node);
+Node *get_left_most_node(Node *node);
+Node *get_right_most_node(Node *node);
+Node *get_root_node(Node *node);
+int get_node_balance(Node *node);
 
 #endif
